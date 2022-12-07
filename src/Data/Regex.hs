6f9283e5bb04a15f642347ce0c8ab6afddaaf4e0
Casper Bach Poulsen <casperbp@gmail.com>
2022-12-07 01:37:57 +0100

    Update frontier to be more accurate

--- src/Data/Regex.hs
+++ src/Data/Regex.hs
@@ -9,6 +9,7 @@ data RE l
   | Pipe (RE l) (RE l)
   | Dot (RE l) (RE l)
   | Star (RE l)
+  deriving Show
 
 -- Computes the list of valid "next" atoms
 frontier :: Eq l => RE l -> [l]
@@ -17,7 +18,9 @@ frontier Stuck = []
 frontier (Atom l) = [l]
 frontier (Pipe r1 r2) = nub $ frontier r1 ++ frontier r2
 frontier (Dot r1 r2) = if isEmpty r1
-  then frontier r2
+  then if isStrictlyEmpty r1
+       then frontier r2
+       else frontier r1 ++ frontier r2
   else frontier r1
 frontier (Star r) = frontier r
 
@@ -30,6 +33,16 @@ isEmpty (Pipe r1 r2) = isEmpty r1 || isEmpty r2
 isEmpty (Dot r1 r2) = isEmpty r1 && isEmpty r2
 isEmpty (Star _) = True
 
+-- Check if regular expression strictly corresponds to the empty language
+isStrictlyEmpty :: RE l -> Bool
+isStrictlyEmpty Empty = True
+isStrictlyEmpty Stuck = False
+isStrictlyEmpty (Atom _) = False
+isStrictlyEmpty (Pipe r1 r2) = isStrictlyEmpty r1 || isStrictlyEmpty r2
+isStrictlyEmpty (Dot r1 r2) = isStrictlyEmpty r1 && isStrictlyEmpty r2
+isStrictlyEmpty (Star _) = False -- !
+
+
 -- Brzozowski derivative of regular expression
 derive :: Eq l => l -> RE l -> RE l
 derive _ Empty = Stuck
@@ -40,7 +53,7 @@ derive l (Pipe r1 r2) = case (derive l r1, derive l r2) of
   (Stuck, r2') -> r2'
   (r1', Stuck) -> r1'
   (r1', r2')   -> Pipe r1' r2'
-derive l (Dot r1 r2) = if isEmpty r1
+derive l (Dot r1 r2) = if isStrictlyEmpty r1
   then derive l r2
   else Dot (derive l r1) r2
 derive l (Star r) = Dot (derive l r) (Star r)
